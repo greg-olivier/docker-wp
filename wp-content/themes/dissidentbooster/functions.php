777c148f850f18e85b507586f13b7666248502a6
Aaron Khochareun <git@utheron.com>
2018-09-09 10:48:40 +0200

    switch to stand alone theme

---
+++ wp-content/themes/dissidentbooster/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function add_normalize_css() {
+	wp_enqueue_style( 'normalize-styles', "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css");
+}
+
+add_action( 'widgets_init', 'add_widget_support' );
+function add_widget_support() {
+	register_sidebar( array(
+		'name'          => 'Sidebar',
+		'id'            => 'sidebar',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2>',
+		'after_title'   => '</h2>',
+	) );
+}
+
+add_action( 'init', 'add_main_nav' );
+function add_main_nav() {
+	register_nav_menu('header-menu',__( 'Header Menu' ));
+}
