25dd17f3f8712ac5f97542e1ff178c6b45d2f38c
Aaron Khochareun <git@utheron.com>
2018-09-09 16:29:48 +0200

    added post-thumbnails

--- wp-content/themes/dissidentbooster/index.php
+++ wp-content/themes/dissidentbooster/index.php
@@ -1,19 +1,19 @@
 <?php get_header(); ?>
-	<main class="wrap">
-		<section class="content-area content-thin">
+    <main class="wrap">
+        <section class="content-area content-thin">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<article class="article-loop">
-					<header>
-						<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-						By: <?php the_author(); ?>
-					</header>
+                <article class="article-loop">
+                    <header>
+                        <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                        By: <?php the_author(); ?>
+                    </header>
 					<?php the_excerpt(); ?>
-				</article>
+                </article>
 			<?php endwhile; else : ?>
-				<article>
-					<p>Sorry, no posts were found!</p>
-				</article>
+                <article>
+                    <p>Sorry, no posts were found!</p>
+                </article>
 			<?php endif; ?>
-		</section><?php get_sidebar(); ?>
-	</main>
+        </section><?php get_sidebar(); ?>
+    </main>
 <?php get_footer(); ?>
